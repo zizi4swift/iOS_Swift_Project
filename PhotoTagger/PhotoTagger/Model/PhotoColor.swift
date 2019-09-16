8075af14877dfe59ac8db3318a707c7881d61f60
setsu <zizi4swift@gmail.com>
2019-09-16 14:50:01 +0900

    写真の色パターンを表す構造体を作成する

---
+++ PhotoTagger/PhotoTagger/Model/PhotoColor.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoColor.swift
+//  PhotoTagger
+//
+//  Created by setsu on 2019/09/16.
+//  Copyright © 2019 setsu. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoColor {
+    let red: Int
+    let green: Int
+    let blue: Int
+    let colorName: String
+}
