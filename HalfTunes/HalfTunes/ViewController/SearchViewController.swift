eddc4e3e0d8d33e1a2ac76a903fe43f34454c0d7
setsu <zizi4swift@gmail.com>
2019-10-20 15:43:33 +0900

    検索バーDelegateメソッドを実装

--- HalfTunes/HalfTunes/ViewController/SearchViewController.swift
+++ HalfTunes/HalfTunes/ViewController/SearchViewController.swift
@@ -14,10 +14,49 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK: - Properties
+    // ジェスチャー認識コンポーネント
+    lazy var tapRecognizer: UITapGestureRecognizer = {
+        var recognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        return recognizer
+    }()
+    
+    // MARK: - View Cycle Life
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - Private Methods
+    
+    /// キーボードを隠すメソッド
+    @objc func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+}
 
-
+// MARK: - Search Bar Delegate
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        dismissKeyboard()
+        
+        // 検索バーに文字は存在するか及び空文字列ではないかを確認する
+        guard let searchText = searchBar.text, !searchText.isEmpty else {
+            return
+        }
+        
+        // インジケーターを表示する
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        // TODO: - 検索結果を取得する機能実装
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        view.addGestureRecognizer(tapRecognizer)
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        
+        view.removeGestureRecognizer(tapRecognizer)
+    }
 }
 
